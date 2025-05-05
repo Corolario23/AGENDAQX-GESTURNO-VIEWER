a413e49adc7fb3c5055f5ec1bc14732f189685b5
Corolario23 <r.caceres.villar@gmail.com>
2025-05-05 14:44:28 -0400

    Update viewer_app.py agregué Database URL

--- viewer_app.py
+++ viewer_app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///turnos.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
